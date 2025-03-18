67ff36060852ee9a06687316901ad558a1cbc6c6
Debsis-Bhuyan <debasisbhuyan2@gmail.com>
2025-03-18 21:57:12 +0530

    update jwt error

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,6 +38,7 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   const { email, password } = req.body;
+  
 
   try {
     //validation
--- utils/index.js
+++ utils/index.js
@@ -3,19 +3,26 @@ import JWT from "jsonwebtoken";
 
 export const hashString = async (useValue) => {
   const salt = await bcrypt.genSalt(10);
-
-  const hashedpassword = await bcrypt.hash(useValue, salt);
-  return hashedpassword;
+  return await bcrypt.hash(useValue, salt);
 };
 
 export const compareString = async (userPassword, password) => {
-  const isMatch = await bcrypt.compare(userPassword, password);
-  return isMatch;
+  return await bcrypt.compare(userPassword, password);
 };
 
 //JSON WEBTOKEN
+
 export function createJWT(id) {
-  return JWT.sign({ userId: id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "1d",
-  });
+  if (!process.env.JWT_SECRET_KEY) {
+    throw new Error("JWT_SECRET_KEY is not defined in environment variables");
+  }
+  try {
+    return JWT.sign({ userId: id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+  } catch (error) {
+    console.error("Error creating JWT:", error);
+    throw new Error("Failed to generate JWT");
+  }
 }
+
